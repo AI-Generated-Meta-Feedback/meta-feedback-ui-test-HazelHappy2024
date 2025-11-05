3935e1803819881a6b03a0b2b527443c6174f347
Hazel Liu <155853728+HazelHappy2024@users.noreply.github.com>
2025-11-05 12:05:56 -0800

    Add sorting function for book list
    Implement insertion sort for a list of books based on shelf number and return order.

--- CODE.py
+++ CODE.py
@@ -1,1 +1,18 @@
+function sortBooks(bookList):
+    #bookList is a lits of (shelfNumber, returnOrder)
+    for i from 1 to length(bookList) - 1:
+      key = bookList[i]
+      j  = i - 1
+      #compare shelfNumber first, then returnOrder if same shelf 
+      while j >= 0 and (
+           bookList[j].shelfNumber > key.shelfNumber or 
+            (bookList[j].shelfNumber == key.shelfNumber and 
+            bookList[j].returnOrder > key.returnOrder)
 
+      ):
+        bookList[j + 1] = bookList[j]
+        j = j - 1
+
+      bookList[j + 1] = key
+
+      return bookList
